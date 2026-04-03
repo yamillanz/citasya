c737b95c9583800a5787a8ce176fed29ec4f8bbd
yamillanz <yamil.lanz@gmail.com>
2026-04-03 09:14:45 -0400

    fix(angular): consitency arround angular 20

--- app-web/src/app/app.config.ts
+++ app-web/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import { MessageService } from 'primeng/api';
-import Aura from '@primeng/themes/aura';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
